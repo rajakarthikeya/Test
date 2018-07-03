46a5ee0d3273933a435b750e053448be319f59c3
RajaKarthikeya <karthikeya9vamsi@gmail.com>
2018-07-03 12:11:08 +0530

    adding sample pattern file

---
+++ DisplayPattern.java
@@ -0,0 +1,33 @@
+
+public class DisplayPattern {
+
+	public static void main(String[] args) {
+		int k = 5;
+		String emptyStr = "";
+		for (int i = 1; i < k; i++) {
+			emptyStr = generateSpaces(i);
+			String rowToPrint = "";
+			for (int j = i; j <= k; j++) {
+				rowToPrint += j;
+			}
+			System.out.println(emptyStr + rowToPrint);
+		}
+		emptyStr = generateSpaces(k);
+		System.out.println(emptyStr + k);
+		for (int i = 1; i < k; i++) {
+			emptyStr = generateSpaces(k-i);
+			String rowToPrint = "";
+			for (int j = k - i; j <= k; j++) {
+				rowToPrint += j;
+			}
+			System.out.println(emptyStr+ rowToPrint);
+		}
+	}
+	private static String generateSpaces(int count) {
+		String s = "";
+		for (int z = 1; z < count; z++) {
+			s += " ";
+		}
+		return s;
+	}
+}
